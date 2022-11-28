9f925a3ce22578ddb8ab5e2b3b5003a0249c4fcd
demohari <kishore_koneru@yahoo.com>
2022-11-28 19:00:35 +0000

    callbacks added

--- src/utils/data_mgmt.py
+++ src/utils/data_mgmt.py
@@ -5,12 +5,18 @@ def get_data(validation_datasize):
     mnist = tf.keras.datasets.mnist
     (X_train_full, y_train_full), (X_test, y_test) = mnist.load_data()
 
-    # create a validation data set from the full training data 
+    # create a validation data set from the full training data
     # Scale the data between 0 to 1 by dividing it by 255. as its an unsigned data between 0-255 range
-    X_valid, X_train = X_train_full[:validation_datasize] / 255., X_train_full[validation_datasize:] / 255.
-    y_valid, y_train = y_train_full[:validation_datasize], y_train_full[validation_datasize:]
+    X_valid, X_train = (
+        X_train_full[:validation_datasize] / 255.0,
+        X_train_full[validation_datasize:] / 255.0,
+    )
+    y_valid, y_train = (
+        y_train_full[:validation_datasize],
+        y_train_full[validation_datasize:],
+    )
 
     # scale the test set as well
-    X_test = X_test / 255.
+    X_test = X_test / 255.0
 
     return (X_train, y_train), (X_valid, y_valid), (X_test, y_test)
